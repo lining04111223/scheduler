4813f8858d18194685b6e53007d55e20d71126f0
Ning Li <lining04111223@gmail.com>
2022-11-02 16:18:41 -0700

    test some change

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,11 +7,12 @@ export default function Button(props) {
    const buttonClass =
    classNames("button", {"button--confirm":props.confirm}, {"button--danger":props.danger});
 
-  return <button 
+  return (<button 
   onClick={props.onClick} 
   className={buttonClass}
   disabled={props.disabled}
   >
      {props.children}
-     </button>;
+     </button>
+  );
 }
