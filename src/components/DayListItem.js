59ab89031b368787de9433a197d46b3b9c5d48ea
Ning Li <lining04111223@gmail.com>
2022-11-02 20:00:23 -0700

    move DayList to app

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,15 +17,15 @@ const formatSpots = function(spot){
 };
 
 const dayClass = classNames("day-list__item", 
-{"day-list__item--selected":props.selected}, 
-{"day-list__item--full":props.spots === 0}
+    {"day-list__item--selected":props.selected}, 
+    {"day-list__item--full":props.spots === 0}
 );
 
   return (
-<li 
-onClick={() => props.setDay(props.name)}
-className={dayClass}
->
+    <li 
+        onClick={() => props.setDay(props.name)}
+        className={dayClass}
+        >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
