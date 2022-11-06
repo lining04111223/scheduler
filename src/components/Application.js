160d60e23a787a3d1f6c40fc14e15adfcd955a66
Ning Li <lining04111223@gmail.com>
2022-11-05 23:21:06 -0700

    Retrieving Interviewer Data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,35 +3,39 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index.js";
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay,getInterview  } from "../helpers/selectors";
 
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers:{}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
   const appoint = dailyAppointments.map((item) =>{
+    const interview = getInterview(state, item.interview);
+
+
       return(
       <Appointment
       key = {item.id}
-      //id = {item.id}
-      //time ={item.time}
-      //interview = {item.interview}
-      {...item}//the same with the 3 lines in front
+      id = {item.id}
+      time ={item.time}
+      interview = {interview}
       />
       )
   });
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({...prev, days:all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days:all[0].data, appointments: all[1].data,interviewers: all[2].data}));
     });
   }, []);
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,17 @@ export function getAppointmentsForDay(state, day) {
   return filteredAppoint[0].appointments.map(ID => state.appointments[ID] )
 }
 
+export function getInterview(state, interview){
+  if (interview) {
+     const interviewerObj = { 
+       student: interview.student,
+       interviewer: {
+        id: interview.interviewer,
+        name: state.interviewers[interview.interviewer].name,
+        avatar: state.interviewers[interview.interviewer].avatar
+      }
+    }
+      return interviewerObj;
+  }
+  return null
+}
